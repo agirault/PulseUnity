6fdf2e0e2ca40547f07b6685d121fa24508ab01b
Alexis Girault <alexis.girault@kitware.com>
2018-11-08 15:38:33 -0500

    Implement PulseEngine and PulseEngineDriver
    PulseEngine: wraps PulseC API in C#
    PulseEngineDriver: PulseDataSource which handles advancing time to
    compute data

---
+++ Scripts/PulseEngine.cs
@@ -0,0 +1,67 @@
+﻿/* Distributed under the Apache License, Version 2.0.
+   See accompanying NOTICE file for details.*/
+
+using System;
+using System.Runtime.InteropServices;
+
+public class PulseEngine
+{
+    public enum SerializationFormat : int { BINARY = 0, ASCII }
+
+    readonly IntPtr pulseEngineCPtr;
+
+    [DllImport("PulseC")]
+    static extern IntPtr Allocate(string logfile);
+    PulseEngine() { } // make default constructor private
+    public PulseEngine(string logFilePath) {
+        pulseEngineCPtr = Allocate(logFilePath);
+    }
+
+    [DllImport("PulseC")]
+    static extern void Deallocate(IntPtr pulse);
+    ~PulseEngine() {
+        Deallocate(pulseEngineCPtr);
+    }
+
+    [DllImport("PulseC")]
+    static extern bool SerializeFromFile(IntPtr pulse, string filename, string data_requests, int format, double sim_time_s);
+    public bool SerializeFromFile(string filename, string data_requests, SerializationFormat format, double sim_time_s) {
+        return SerializeFromFile(pulseEngineCPtr, filename, data_requests, (int)format, sim_time_s);
+    }
+
+    [DllImport("PulseC")]
+    static extern bool SerializeToFile(IntPtr pulse, string filename, int format);
+    public bool SerializeToFile(string filename, SerializationFormat format) {
+        return SerializeToFile(pulseEngineCPtr, filename, (int)format);
+    }
+
+    [DllImport("PulseC", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    static extern bool SerializeFromString(IntPtr pulse, string state, string data_requests, int format, double sim_time_s);
+    public bool SerializeFromString(string state, string data_requests, SerializationFormat format, double sim_time_s) {
+        return SerializeFromString(pulseEngineCPtr, state, data_requests, (int)format, sim_time_s);
+    }
+
+    [DllImport("PulseC", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    static extern string SerializeToString(IntPtr pulse, int format);
+    public string SerializeToString(SerializationFormat format) {
+        return SerializeToString(pulseEngineCPtr, (int)format);
+    }
+
+    [DllImport("PulseC", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    static extern void InitializeEngine(IntPtr pulse, string patient_configuration, string data_requests, int format);
+    public void InitializeEngine(string patient_configuration, string data_requests, SerializationFormat format) {
+        InitializeEngine(pulseEngineCPtr, patient_configuration, data_requests, (int)format);
+    }
+
+    [DllImport("PulseC", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    static extern bool AdvanceTime_s(IntPtr pulse, double time);
+    public bool AdvanceTime_s(double timeStep) {
+        return AdvanceTime_s(pulseEngineCPtr, timeStep);
+    }
+
+    [DllImport("PulseC", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    static extern IntPtr PullData(IntPtr pulse);
+    public IntPtr PullData() {
+        return PullData(pulseEngineCPtr);
+    }
+}
---
+++ Scripts/PulseEngineDriver.cs
@@ -0,0 +1,108 @@
+﻿/* Distributed under the Apache License, Version 2.0.
+   See accompanying NOTICE file for details.*/
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PulseEngineDriver: PulseDataSource
+{
+    public TextAsset initialStateFile;
+    public PulseEngine.SerializationFormat serializationFormat;
+
+    [Range(0.02f, 2.0f)]
+    public double timeStep = 0.02;
+
+    [HideInInspector, NonSerialized]
+    public PulseEngine engine;
+
+    float previousTime;
+
+    readonly string[] pulseDataFields = {
+        "SimTime (s)",
+        "Lead3ElectricPotential (mV)",
+        "HeartRate (1\u2215min)",
+        "ArterialPressure (mmHg)",
+        "MeanArterialPressure (mmHg)",
+        "SystolicArterialPressure (mmHg)",
+        "DiastolicArterialPressure (mmHg)",
+        "OxygenSaturation",
+        "EndTidalCarbonDioxidePressure (mmHg)",
+        "RespirationRate (1\u2215min)",
+        "SkinTemperature (degC)",
+        "Carina-CarbonDioxide-PartialPressure (mmHg)"
+    };
+
+    void OnValidate() {
+        // Round down to closest factor of 0.02. Need to use doubles due to
+        // issues with floats multiplication (0.1 -> 0.0999999)
+        timeStep = Math.Round(timeStep / 0.02) * 0.02;
+    }
+
+    void Awake() {
+        if (data != null) return;
+        data = new PulseData();
+        data.fields = pulseDataFields;
+        data.timeStampList = new FloatList();
+        data.valuesTable = new List<FloatList>(pulseDataFields.Length);
+        for (int fieldId = 0; fieldId < pulseDataFields.Length; ++fieldId) {
+            data.valuesTable.Add(new FloatList());
+        }
+    }
+
+    void Start () {
+        if (!Application.isPlaying || initialStateFile == null) return;
+
+        // Allocate PulseEngine with log file path
+        string dateAndTimeVar = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+        string logFilePath = Application.persistentDataPath + "/" +
+                                        gameObject.name +
+                                        dateAndTimeVar + ".log";
+        engine = new PulseEngine(logFilePath);
+
+        // Initialize engine state from string
+        engine.SerializeFromString(initialStateFile.text,
+                                   null, // requested data currently hardcoded
+                                   serializationFormat,
+                                   Time.time);
+
+        previousTime = Time.time;
+    }
+
+	void Update () {
+        if (!Application.isPlaying || engine == null) return;
+
+        // Clear PulseData container
+        data.timeStampList.Clear();
+        for (int j = 0; j < data.valuesTable.Count; ++j) {
+            data.valuesTable[j].Clear();
+        }
+
+        // Don't advance simulation if we have waited less than the time step
+        float timeElapsed = Time.time - previousTime;
+        if (timeElapsed < timeStep) return;
+
+        // Iterate over multiple time steps if needed
+        var numberOfDataPointsNeeded = Math.Round(timeElapsed / timeStep);
+        for (int i = 0; i < numberOfDataPointsNeeded; ++i) {
+            // Increment previousTime to currentTime (factored by the time step)
+            previousTime += (float)timeStep;
+            data.timeStampList.Add(previousTime);
+
+            // Advance simulation by time step
+            bool success = engine.AdvanceTime_s(timeStep);
+            if (!success) continue;
+
+            // Copy result data
+            IntPtr results = engine.PullData();
+            int nbrOfValues = data.valuesTable.Count;
+            double[] array = new double[nbrOfValues];
+            Marshal.Copy(results, array, 0, nbrOfValues);
+            for (int j = 0; j < nbrOfValues; ++j) {
+                data.valuesTable[j].Add((float)array[j]);
+            }
+        }
+	}
+}
