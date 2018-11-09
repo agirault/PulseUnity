c85ccbae65fb4b1903b032f5cf2d3be47b90167e
Alexis Girault <alexis.girault@kitware.com>
2018-11-09 13:35:38 -0500

    Separate custom editors in Editor folder

---
+++ Editor/PulseDataConsumerEditor.cs
@@ -0,0 +1,52 @@
+﻿/* Distributed under the Apache License, Version 2.0.
+   See accompanying NOTICE file for details.*/
+
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PulseDataConsumer), true)]
+public class PulseDataConsumerEditor : Editor
+{
+    SerializedProperty sourceProp;
+    SerializedProperty dataFieldIndexProp;
+
+    void OnEnable()
+    {
+        sourceProp = serializedObject.FindProperty("source");
+        dataFieldIndexProp = serializedObject.FindProperty("dataFieldIndex");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        // Select generator
+        EditorGUILayout.PropertyField(sourceProp, new GUIContent("Data source"));
+
+        var source = sourceProp.objectReferenceValue as PulseDataSource;
+        if (source == null || source.data == null || source.data.fields == null)
+        {
+            dataFieldIndexProp.intValue = 0;
+            serializedObject.ApplyModifiedProperties();
+            EditorGUILayout.LabelField("Error",
+                                       source == null ?
+                                       "Data source missing" :
+                                       "Data source could not generate valid data fields");
+            return;
+        }
+
+        // Select datafield
+        string[] fields = source.data.fields;
+        dataFieldIndexProp.intValue = Mathf.Clamp(dataFieldIndexProp.intValue,
+                                                  0,
+                                                  fields.Length - 1);
+        dataFieldIndexProp.intValue = EditorGUILayout.Popup("Data field",
+                                                            dataFieldIndexProp.intValue,
+                                                            fields);
+
+        // Show default inspector property editor
+        DrawPropertiesExcluding(serializedObject, "m_Script");
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Scripts/PulseDataConsumer.cs
+++ Scripts/PulseDataConsumer.cs
@@ -1,9 +1,7 @@
 ﻿/* Distributed under the Apache License, Version 2.0.
    See accompanying NOTICE file for details.*/
 
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 public abstract class PulseDataConsumer : MonoBehaviour
 {
@@ -31,48 +29,4 @@ public abstract class PulseDataConsumer : MonoBehaviour
     }
 
     abstract internal void UpdateFromPulse(FloatList times, FloatList values);
-}
-
-[CustomEditor(typeof(PulseDataConsumer), true)]
-public class PulseDataConsumerEditor : Editor
-{
-    SerializedProperty sourceProp;
-    SerializedProperty dataFieldIndexProp;
-
-    void OnEnable() {
-        sourceProp = serializedObject.FindProperty("source");
-        dataFieldIndexProp = serializedObject.FindProperty("dataFieldIndex");
-    }
-
-    public override void OnInspectorGUI() {
-        serializedObject.Update();
-
-        // Select generator
-        EditorGUILayout.PropertyField(sourceProp, new GUIContent("Data source"));
-
-        var source = sourceProp.objectReferenceValue as PulseDataSource;
-        if (source == null || source.data == null || source.data.fields == null) {
-            dataFieldIndexProp.intValue = 0;
-            serializedObject.ApplyModifiedProperties();
-            EditorGUILayout.LabelField("Error",
-                                       source == null ?
-                                       "Data source missing" :
-                                       "Data source could not generate valid data fields");
-            return;
-        }
-
-        // Select datafield
-        string[] fields = source.data.fields;
-        dataFieldIndexProp.intValue = Mathf.Clamp(dataFieldIndexProp.intValue,
-                                                  0,
-                                                  fields.Length - 1);
-        dataFieldIndexProp.intValue = EditorGUILayout.Popup("Data field",
-                                                            dataFieldIndexProp.intValue,
-                                                            fields);
-
-        // Show default inspector property editor
-        DrawPropertiesExcluding(serializedObject, "m_Script");
-
-        serializedObject.ApplyModifiedProperties();
-    }
 }
